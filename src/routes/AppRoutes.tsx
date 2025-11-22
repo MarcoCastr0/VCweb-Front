1baf4b4082df6e8ca99967f066bc43308bf4ee70
Marco Fidel Castro Velasco <marco.castro@correounivalle.edu.co>
2025-11-22 13:54:16 -0500

    add modal confirmation to eliminate acount

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,3 +1,8 @@
+/**
+ * @file AppRoutes.tsx
+ * @description Defines all application routes using React Router.
+ */
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
@@ -10,6 +15,12 @@ import Sitemap from '../pages/Sitemap';
 import EditProfile from '../pages/EditProfile';
 import VideoCall from '../pages/Video_call';
 
+/**
+ * AppRoutes component that defines all available frontend routes.
+ *
+ * @component
+ * @returns {JSX.Element} Routes configuration for the application.
+ */
 const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -18,13 +29,12 @@ const AppRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
-        <Route path='/forgot-password' element={<ForgotPassword />}/>
-        <Route path='/start-meeting' element={<StartMeeting />}/>
-        <Route path='/profile' element={<Profile />}/>
-        <Route path='/sitemap' element={<Sitemap />}/>
-        <Route path='/edit-profile' element={<EditProfile />}/>
-        <Route path='/video-call' element={<VideoCall />}/>
-
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/start-meeting' element={<StartMeeting />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sitemap' element={<Sitemap />} />
+        <Route path='/edit-profile' element={<EditProfile />} />
+        <Route path='/video-call' element={<VideoCall />} />
       </Routes>
     </BrowserRouter>
   );
